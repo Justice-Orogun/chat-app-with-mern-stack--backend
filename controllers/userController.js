c484f130bfdd232db63f18dc5b86df6a9ef4322c
James Serengia <jamesserengia@gmail.com>
2023-05-08 21:02:09 +0300

    Add error comtroller

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,1 +1,15 @@
 const User = require("../models/User");
+
+exports.getAllUsers = async (req, res, next) => {
+  try {
+    const users = await User.find();
+    res.status(200).json({
+      status: "success",
+      data: {
+        users,
+      },
+    });
+  } catch (error) {
+    next(error);
+  }
+};
--- router/usersRouter.js
+++ router/usersRouter.js
@@ -1,7 +1,8 @@
 const express = require("express");
-const router = express.Router();
-const authController = require("../controllers/authController");
 
-router.route("/").post(authController.signup);
+const router = express.Router();
+const userController = require("../controllers/userController");
+
+router.route("/").get(userController.getAllUsers);
 
 module.exports = router;
