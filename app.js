1ff425eef3bb2021c91e1b4feca4ec8da8e03d35
James Serengia <jamesserengia@gmail.com>
2023-05-08 07:06:13 +0300

    Updated socket it with cors

--- app.js
+++ app.js
@@ -1,27 +1,18 @@
-const express = require("express");
-const app = express();
-const http = require("http");
-const server = http.createServer(app);
-const io = require("socket.io")(server);
+const app = require("express")();
+const server = require("http").createServer(app);
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "http://localhost:3000",
+  },
+});
 
 require("dotenv").config();
 
-// Your Express routes
-app.get("/", (req, res) => {
-  res.send("Hello, world!");
+io.on("connection", () => {
+  console.log("CONNECTION WORKING");
 });
 
-// Socket.io events
-io.on("connection", (socket) => {
-  console.log("A user connected");
-
-  socket.on("disconnect", () => {
-    console.log("A user disconnected");
-  });
-});
-
-// Start the server
 const PORT = process.env.PORT || 8001;
 server.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
+  console.log(`Server running on port ${PORT} ...`);
 });
